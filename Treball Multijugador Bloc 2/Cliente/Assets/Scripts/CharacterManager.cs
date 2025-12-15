e080b9eeb78f2bed72e796aa2eed5b322cde58f4
Robert <robertlopezcontreras@gmail.com>
2025-12-15 17:18:40 +0100

    Cliente update

--- Treball Multijugador Bloc 2/Cliente/Assets/Scripts/CharacterManager.cs
+++ Treball Multijugador Bloc 2/Cliente/Assets/Scripts/CharacterManager.cs
@@ -8,35 +8,36 @@ using UnityEngine.UI;
 public class CharacterManager : MonoBehaviour
 {
 
+    public Transform perro;
+
+    public Transform creeper;
+
     private void Start()
     {
-        GameObject canvas = GameObject.Find("Canvas");
         float posicionXPerro = ClientBehaviour.Instance.posXPerro;
         float posicionYPerro = ClientBehaviour.Instance.posYPerro;
         float posicionXCreeper = ClientBehaviour.Instance.posXCreeper;
         float posicionYCreeper = ClientBehaviour.Instance.posYCreeper;
 
-        Transform perroTransform = canvas.transform.Find("perroPersonaje");
-        Transform creeperTransform = canvas.transform.Find("creeperPersonaje");
         //Debug.Log(posicionX);
         //Debug.Log(posicionY);
-        perroTransform.gameObject.SetActive(true);
-        perroTransform.transform.position = new Vector3(posicionXPerro, posicionYPerro, 0);
-        creeperTransform.gameObject.SetActive(true);
-        creeperTransform.transform.position = new Vector3(posicionXCreeper, posicionYCreeper, 0);
+        perro.gameObject.SetActive(true);
+        perro.transform.position = new Vector3(posicionXPerro, posicionYPerro, 0);
+        creeper.gameObject.SetActive(true);
+        creeper.transform.position = new Vector3(posicionXCreeper, posicionYCreeper, 0);
 
         if (ClientBehaviour.Instance.perro == true) {
 
             Debug.Log("Perro visto");
 
 
-            if (perroTransform != null)
+            if (perro != null)
             {
             }
         }
         if (ClientBehaviour.Instance.creeper == true)
         {
-            if (creeperTransform != null)
+            if (creeper != null)
             {
 
             }
